fe6aef027310fce458c69def929d96a34be2f5a3
z01nl1o02 <z0n1l@outlook.com>
2018-11-11 10:39:18 +0800

    support ./darknet detector recall2

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-./darknet detector test examples/traffic/image.data examples/traffic/yolov3.cfg examples/traffic/yolov3.backup data/bike.jpg 
+#./darknet detector test examples/traffic/image.data examples/traffic/yolov3.cfg examples/traffic/yolov3_900.weights data/person_walk.jpg 
+
+./darknet detector recall2 examples/traffic/image.data examples/traffic/yolov3.cfg examples/traffic/yolov3_900.weights examples/traffic/test.txt 
